149fb3ba7fb51318c874a6f370d56193bcdd649f
raafstone2 <raafstone2@gmail.com>
2026-08-02 01:22:42 +0330

    Update checker.py

--- checker.py
+++ checker.py
@@ -11,13 +11,13 @@ headers = {
 
 
 def get_last_message():
-    r = requests.get(
+    response = requests.get(
         CHANNEL_URL,
         headers=headers,
         timeout=20
     )
 
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
 
     messages = soup.find_all(
         "div",
@@ -30,32 +30,34 @@ def get_last_message():
     return None
 
 
-def load_old_message():
+def read_old_message():
     if os.path.exists(LAST_FILE):
-        with open(LAST_FILE, "r", encoding="utf-8") as f:
-            return f.read()
+        with open(LAST_FILE, "r", encoding="utf-8") as file:
+            return file.read()
 
     return ""
 
 
 def save_message(message):
-    with open(LAST_FILE, "w", encoding="utf-8") as f:
-        f.write(message)
+    with open(LAST_FILE, "w", encoding="utf-8") as file:
+        file.write(message)
 
 
 new_message = get_last_message()
 
-if new_message:
-    old_message = load_old_message()
+if new_message is None:
+    print("خطا در دریافت پیام کانال")
+    exit()
 
-    if new_message != old_message:
-        print("پیام جدید پیدا شد:")
-        print(new_message)
 
-        save_message(new_message)
+old_message = read_old_message()
 
-    else:
-        print("پیام جدیدی وجود ندارد")
+
+if new_message != old_message:
+    print("پیام جدید پیدا شد:")
+    print(new_message)
+
+    save_message(new_message)
 
 else:
-    print("نتوانستم پیام کانال را دریافت کنم")
+    print("پیام جدیدی وجود ندارد")
