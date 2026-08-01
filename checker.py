3c735bc49cf9c971dbb3a1e969f9ec30d4dd7d25
raafstone2 <raafstone2@gmail.com>
2026-08-02 01:34:24 +0330

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,23 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 CHANNEL_URL = "https://t.me/s/v2raytun_kanfing"
 LAST_FILE = "last_message.txt"
+
+GMAIL_USER = os.environ.get("GMAIL_USER")
+GMAIL_TO = os.environ.get("GMAIL_TO")
+GMAIL_APP_PASSWORD = os.environ.get("GMAIL_APP_PASSWORD")
+
 
 headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
 
-def get_last_message():
-    response = requests.get(
+def get_latest_message():
+    r = requests.get(
         CHANNEL_URL,
         headers=headers,
         timeout=20
     )
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser")
 
     messages = soup.find_all(
         "div",
@@ -32,18 +41,57 @@ def get_last_message():
 
 def read_old_message():
     if os.path.exists(LAST_FILE):
-        with open(LAST_FILE, "r", encoding="utf-8") as file:
-            return file.read()
+        with open(LAST_FILE, "r", encoding="utf-8") as f:
+            return f.read()
 
     return ""
 
 
 def save_message(message):
-    with open(LAST_FILE, "w", encoding="utf-8") as file:
-        file.write(message)
+    with open(LAST_FILE, "w", encoding="utf-8") as f:
+        f.write(message)
 
 
-new_message = get_last_message()
+def send_email(message):
+
+    msg = MIMEMultipart()
+
+    msg["From"] = GMAIL_USER
+    msg["To"] = GMAIL_TO
+    msg["Subject"] = "کانفیگ جدید V2Ray از تلگرام"
+
+    body = """
+کانفیگ جدید در کانال تلگرام پیدا شد:
+
+---------------------
+
+""" + message
+
+    msg.attach(
+        MIMEText(body, "plain", "utf-8")
+    )
+
+    server = smtplib.SMTP_SSL(
+        "smtp.gmail.com",
+        465
+    )
+
+    server.login(
+        GMAIL_USER,
+        GMAIL_APP_PASSWORD
+    )
+
+    server.sendmail(
+        GMAIL_USER,
+        GMAIL_TO,
+        msg.as_string()
+    )
+
+    server.quit()
+
+
+new_message = get_latest_message()
+
 
 if new_message is None:
     print("خطا در دریافت پیام کانال")
@@ -54,10 +102,17 @@ old_message = read_old_message()
 
 
 if new_message != old_message:
+
     print("پیام جدید پیدا شد:")
     print(new_message)
 
     save_message(new_message)
 
+    send_email(new_message)
+
+    print("ایمیل ارسال شد")
+
+
 else:
+
     print("پیام جدیدی وجود ندارد")
