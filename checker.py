c3001c72cdc4f91ffe9f3e42fb30915a246185b2
raafstone2 <raafstone2@gmail.com>
2026-08-02 00:39:54 +0330

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://t.me/s/v2raytun_kanfing"
 
@@ -8,5 +9,15 @@ headers = {
 
 r = requests.get(url, headers=headers, timeout=20)
 
-print("Status:", r.status_code)
-print(r.text[:1000])
+soup = BeautifulSoup(r.text, "html.parser")
+
+messages = soup.find_all("div", class_="tgme_widget_message_text")
+
+print("تعداد پیام پیدا شده:", len(messages))
+
+if messages:
+    last_message = messages[-1].get_text("\n", strip=True)
+    print("آخرین پیام کانال:")
+    print(last_message)
+else:
+    print("پیامی پیدا نشد")
